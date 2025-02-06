718aaf0fd136c3249b797c8dc950e09ee841929e
Jasmine John <jsjohn026@gmail.com>
2025-02-06 14:05:40 -0600

    adjusted server side javascript

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.get('/api/:plantName', (req, res) => { // colon lets express know that the q
 
   if(plants[plantsName]) {
     res.json(plants[plantsName])
-    document.querySelector('h2').innerText = plants[plantsName].cultivar + ' ' + plants[plantsName].name
   } else {
     res.json(plants['unknown'])
   }
